829a8a69f82b9699cf2d8657fb3c3ce606d28438
翁莞婷 <18475969926@163.com>
2020-04-10 20:33:29 +0800

    提交

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.post('/api/admin/upload',upload.array('file'), async (req, res) => {
 
 //处理跨域
 app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://8.129.211.213');
-  // res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8082');
+  // res.header('Access-Control-Allow-Origin', 'http://8.129.211.213');
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8082');
   res.header('Access-Control-Allow-Headers', 'X-Token, Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials','true');   // 新增
