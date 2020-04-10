8a1b2f5369e85fce3929b6b54d85f66e159c2337
翁莞婷 <18475969926@163.com>
2020-04-10 22:42:39 +0800

    提交

--- server/index.js
+++ server/index.js
@@ -23,19 +23,14 @@ app.post('/api/admin/upload',upload.array('file'), async (req, res) => {
 })
 
 //处理跨域
-app.all('*',function (req, res, next) {
-  // res.header('Access-Control-Allow-Origin', 'http://8.129.211.213');
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8082');
-  res.header('Access-Control-Allow-Headers', 'X-Token, Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials','true');   // 新增
-  if (req.method == 'OPTIONS') {
-    res.sendStatus(200); /*让options请求快速返回*/
-  }
-  else {
-    next();
-  }
-})
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 
 // 后端api路由
 app.use('/api/admin', adminApi);
